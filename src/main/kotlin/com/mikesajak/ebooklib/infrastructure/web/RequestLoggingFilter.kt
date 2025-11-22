fcbccee56e9c699747287e12fd00f6e255f7dd6c
Mike Sajak <mike.sajak@gmail.com>
2025-11-22 17:44:53 +0100

    [BE] Add request logging filter

---
+++ src/main/kotlin/com/mikesajak/ebooklib/infrastructure/web/RequestLoggingFilter.kt
@@ -0,0 +1,61 @@
+package com.mikesajak.ebooklib.infrastructure.web
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.slf4j.LoggerFactory
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import java.lang.System.currentTimeMillis
+import java.util.*
+
+@ConfigurationProperties("app.request-logging")
+data class RequestLoggingProperties(
+        val enabled: Boolean = true,
+        val logHeaders: Boolean = false,
+        val maskedHeaders: Set<String> = setOf("authorization", "proxy-authorization")
+)
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@EnableConfigurationProperties(RequestLoggingProperties::class)
+class RequestLoggingFilter(private val requestLoggingProperties: RequestLoggingProperties) : OncePerRequestFilter() {
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    override fun doFilterInternal(request: HttpServletRequest,
+                                  response: HttpServletResponse,
+                                  filterChain: FilterChain
+    ) {
+        if (!requestLoggingProperties.enabled) {
+            filterChain.doFilter(request, response)
+            return
+        }
+
+        val startTime = currentTimeMillis()
+
+        filterChain.doFilter(request, response)
+
+        val duration = currentTimeMillis() - startTime
+
+        val headers = if (requestLoggingProperties.logHeaders) formatHeaders(request) else "disabled"
+
+        log.debug("Request: [method={}, uri={}, query={}, status={}, duration={}ms, headers={}]",
+                  request.method, request.requestURI, request.queryString, response.status, duration, headers)
+    }
+
+    private fun formatHeaders(request: HttpServletRequest): String {
+        return Collections.list(request.headerNames)
+                .joinToString(", ") { headerName -> "$headerName=${getHeaderValue(headerName, request)}" }
+    }
+
+    private fun getHeaderValue(headerName: String, request: HttpServletRequest): String? =
+        if (shouldMask(headerName)) "***"
+        else request.getHeader(headerName)
+
+    private fun shouldMask(headerName: String): Boolean =
+        requestLoggingProperties.maskedHeaders.contains(headerName.lowercase(Locale.getDefault()))
+}
